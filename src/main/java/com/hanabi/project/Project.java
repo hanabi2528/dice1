44f9c65120ddcae0b59c54e59261cc7b889cc075
hanabi2528 <hanabi191211@gmail.com>
2024-02-23 22:14:00 +0900

    簡易的な丁半を作った

---
+++ src/main/java/com/hanabi/project/Project.java
@@ -0,0 +1,126 @@
+package com.hanabi.project;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Project extends JavaPlugin {
+
+    @Override
+    public void onEnable() {    //起動したときの処理
+        Bukkit.getLogger().info("プラグインが起動しました");
+    }
+
+    @Override
+    public void onDisable() {   Bukkit.getLogger().info("プラグインが終了しました");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) { //入力されたコマンドがdiceかを判定
+        if (command.getName().equalsIgnoreCase("dice")) {
+            Player player_sender = (Player) sender;
+            String name = player_sender.getName();
+
+            if (args.length == 0) {
+                Bukkit.getServer().broadcastMessage("面の数を入力してください");
+                return false;
+            }
+
+            //argsの文字数をlengthで数える
+            if (args.length == 1) {
+                try { //string型の変数をint型に変更
+                    int maxnumber = Integer.parseInt(args[0]);
+                    if (maxnumber > 1000000) {
+                        Bukkit.getServer().broadcastMessage("面の数は1~1000000の整数にしてください");
+                        return false;
+                    }
+
+                    if (maxnumber >= 1) {
+                        //math.randomで0~1の乱数をつくり、面数（maxnumber）でかけて、math.ceilで繰り上げ
+                        int number = (int) Math.ceil(Math.random() * maxnumber);
+                        Bukkit.getServer().broadcastMessage(name + "は" + maxnumber + "面サイコロを振り" + number + "を出しました");
+
+                        return true;
+                    } else {
+                        Bukkit.getServer().broadcastMessage("面の数は1以上の整数にしてください");
+                        return false;
+
+                    }
+                } catch (NumberFormatException e) {
+                    Bukkit.getServer().broadcastMessage("面の数は1以上の整数にしてください");
+                    return false;
+                }
+            }
+        }
+
+        if (command.getName().equalsIgnoreCase("mc")) {
+            Player player_sender = (Player) sender;
+            String name = player_sender.getName();
+
+            if (args.length == 0) {
+                Bukkit.getServer().broadcastMessage("丁の場合は1、半の場合は2を入力してください");
+                return false;
+            }
+
+            if (args.length == 1) {
+                try {
+                    int number1 = Integer.parseInt(args[0]);
+                    if (number1 == 1) {
+                        int chou = (int) Math.ceil(Math.random() * 6) + (int) Math.ceil(Math.random() * 6);
+                        if (chou % 2 == 0) {
+                            Bukkit.getServer().broadcastMessage("丁が出ました！");
+                            sender.sendMessage("当たり！！！");
+                            return true;
+                        }
+                        if (chou % 2 == 1) {
+                            Bukkit.getServer().broadcastMessage("半が出ました！");
+                            sender.sendMessage("はずれ...");
+                            return true;
+                        }
+                    }
+                    if (number1 == 2) {
+                        int han = (int) Math.ceil(Math.random() * 6) + (int) Math.ceil(Math.random() * 6);
+                        if (han % 2 == 0) {
+                            Bukkit.getServer().broadcastMessage("丁が出ました！");
+                            sender.sendMessage("はずれ...");
+                            return true;
+                        }
+                        if (han % 2 == 1) {
+                            Bukkit.getServer().broadcastMessage("半が出ました！");
+                            sender.sendMessage("当たり！！！");
+                            return true;
+                        }
+                    }
+                } catch (NumberFormatException e) {
+                    Bukkit.getServer().broadcastMessage("丁か半かを選んでください");
+                    return false;
+
+                }
+
+            }
+        }
+        return false;// falseを返す
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
